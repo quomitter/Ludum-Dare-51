93b1c971f1da083aaa9f98e74ec20860e75ad62e
quomitter <109629602+quomitter@users.noreply.github.com>
2022-10-01 19:53:52 -0400

    update

--- Assets/InTheAir.cs
+++ Assets/InTheAir.cs
@@ -6,22 +6,29 @@ public class InTheAir : MonoBehaviour
 {
     public bool ropeOnGround;
     public int jumpCounter;
+    public SpriteRenderer spriteRenderer; 
+    public AudioSource audioSource;
+    public AudioClip hitRopeSound; 
     
     private ManController manController; 
 
     // Start is called before the first frame update
     void Start()
     {
-        manController = FindObjectOfType<ManController>(); 
+        manController = FindObjectOfType<ManController>();
+        spriteRenderer = GetComponent<SpriteRenderer>();  
     }
 
     // Update is called once per frame
     void Update()
     {
+        spriteRenderer.color = Color.white; 
         if(ropeOnGround && !manController.isJumping){
             //missed jump
             Debug.Log("missed jump");
             jumpCounter = jumpCounter - 1; 
+            spriteRenderer.color = Color.red; 
+            AudioSource.PlayClipAtPoint(hitRopeSound,new Vector3(-7f,3f,0f));
         }
         if(ropeOnGround && manController.isJumping){
             //made jump
